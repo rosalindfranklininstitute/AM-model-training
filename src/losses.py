6dd2106ea6acef7bec7d1e78e7a97d882b977298
Thomas Fish <50103643+thomasmfish@users.noreply.github.com>
2025-02-28 16:17:33 +0000

    split out losses

---
+++ src/losses.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+import typing
+
+from torch import Tensor
+from monai import losses
+
+if typing.TYPE_CHECKING:
+    from torch import DeviceObjType
+    from collections.abc import Callable, Sequence
+
+
+def weights_to_tensor(weights: Sequence[float], device: DeviceObjType) -> Tensor:
+    return Tensor(weights).to(device)
+
+
+def diceloss(weights: Tensor, **kwargs) -> losses.DiceLoss:
+    return losses.DiceLoss(
+        include_background=True,
+        sigmoid=True,
+        to_onehot_y=False,
+        weight=weights,
+    )
+
+
+def diceceloss(weights: Tensor, **kwargs) -> losses.DiceCELoss:
+    return losses.DiceCELoss(
+        include_background=True,
+        sigmoid=True,
+        to_onehot_y=False,
+        weight=weights,
+    )
+
+
+def softdicecldiceloss(**kwargs) -> losses.SoftDiceclDiceLoss:
+    return losses.SoftDiceclDiceLoss()
+
+
+def naclloss(num_classes: int, **kwargs) -> losses.NACLLoss:
+    return losses.NACLLoss(num_classes, dim=2, kernel_size=5)
+
+
+loss_creation_functions: dict[str, Callable[..., losses._Loss]] = {
+    "diceloss": diceloss,
+    "diceceloss": diceceloss,
+    "softdicecldiceloss": softdicecldiceloss,
+    "naclloss": naclloss,
+}
