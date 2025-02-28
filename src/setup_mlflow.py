95775593d0a1c16d7eda761fe8fb0a373976e7d4
Thomas Fish <50103643+thomasmfish@users.noreply.github.com>
2025-02-28 16:17:45 +0000

    Add MLFlow integration bits

---
+++ src/setup_mlflow.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import monai
+from monai.handlers import MLFlowHandler
+
+
+def setup_mlflow(
+    tracking_uri: str,
+    training_engine,
+    evaluation_engine,
+) -> None:
+    train_mlflow_handler = MLFlowHandler(
+        tracking_uri=tracking_uri,
+        experiment_name="ConfigWorkflowExperiment",
+        run_name="Training",
+        tag_name="train_loss",
+        iteration_log=True,
+        epoch_log=True,
+        output_transform=monai.handlers.from_engine(["loss"], first=True),
+        close_on_complete=True,
+    )
+    val_mlflow_handler = MLFlowHandler(
+        tracking_uri=tracking_uri,
+        experiment_name="ConfigWorkflowExperiment",
+        run_name="ConfigWorkflowrun1",
+        iteration_log=False,
+    )
+
+    train_mlflow_handler.attach(training_engine)
+    val_mlflow_handler.attach(evaluation_engine)
