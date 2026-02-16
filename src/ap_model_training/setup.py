ea9cec10e9629ba647d6aa1d3a17e5d39fe9da0c
Thomas Fish <50103643+thomasmfish@users.noreply.github.com>
2026-02-16 15:51:42 +0000

    Fix loss typing

--- src/ap_model_training/setup.py
+++ src/ap_model_training/setup.py
@@ -255,7 +255,7 @@ class TrainingObjects:
     validation_data: data.Dataset
     device: torch.device
     model: torch.nn.Module
-    loss_function: _Loss
+    loss_function: Callable[[torch.Tensor, torch.Tensor], torch.Tensor]
     optimizer: torch.optim.Optimizer
     lr_scheduler: torch.optim.lr_scheduler.LRScheduler
     train_metrics: Metrics
@@ -301,7 +301,7 @@ def setup_training_objects(
     validation_data: data.Dataset,
     num_classes: int,
     model: torch.nn.Module,
-    loss_function: _Loss,
+    loss_function: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
     training_batch_size: int,
     validation_batch_size: int,
     num_training_workers: int | None = None,
