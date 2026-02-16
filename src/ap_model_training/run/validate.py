0ab704a5390acfd2442b0304131728c4e5585518
Thomas Fish <50103643+thomasmfish@users.noreply.github.com>
2026-02-16 15:34:19 +0000

    Setup model validation

--- src/ap_model_training/run/validate.py
+++ src/ap_model_training/run/validate.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 import logging
 import typing
+from pathlib import Path
+from contextlib import nullcontext
 from importlib.metadata import distributions
 
+import pandas as pd
 import numpy as np
 
 import torch
@@ -25,8 +28,15 @@ from monai.data import decollate_batch
 
 from ap_model_training.utils import MONAI_KEYS
 from ap_model_training.metrics import MetricsOutput, Metrics
-from ap_model_training.run.utils import get_metrics_to_log, get_model_artifact_path
-
+from ap_model_training.run.utils import (
+    clear_memory,
+    get_metrics_to_log,
+    get_model_artifact_path,
+)
+from ap_model_training.run.mlflow import (
+    log_training_objects_to_mlflow,
+    submit_validation_images_to_mflow,
+)
 
 if typing.TYPE_CHECKING:
     from os import PathLike
@@ -47,6 +57,123 @@ def get_requirements() -> list[str]:
 
 
 PIP_REQUIREMENTS = get_requirements()
+
+
+def run(
+    training_objects: TrainingObjects,
+    training_parameters: TrainingParameters,
+    log_mlflow: bool = False,
+) -> None:
+    model_path = Path(training_parameters.model_path)
+
+    pd.DataFrame(training_objects.training_data.data).to_csv(
+        model_path.with_name(f"{model_path.stem}_train_data.csv")
+    )
+    pd.DataFrame(training_objects.validation_data.data).to_csv(
+        model_path.with_name(f"{model_path.stem}_val_data.csv")
+    )
+
+    with mlflow.start_run() if log_mlflow else nullcontext():
+        if log_mlflow:
+            # log model to mlflow
+            input_array = np.random.uniform(
+                size=(
+                    1,
+                    training_parameters.num_channels,
+                    *training_parameters.input_image_shape,
+                )
+            ).astype(np.float32)
+            model_signature = mlflow.models.infer_signature(
+                input_array,
+                training_objects.model(
+                    torch.from_numpy(input_array).to(training_objects.device)
+                ).numpy(force=True),
+            )
+            del input_array
+
+            mlflow.log_params(training_parameters.asdict(include_metrics=False))
+
+            log_training_objects_to_mlflow(training_objects)
+        else:
+            model_signature = None
+
+        if training_parameters.frozen_epochs > 0:
+            # Freeze model if some initial epochs will be frozen
+            for param in training_objects.model.encoder.parameters():  # type: ignore
+                param.requires_grad = False
+
+        val_epoch_metrics_dict: dict[int, MetricsOutput] = {}
+        epoch: int = 1
+        for epoch in tqdm(
+            range(training_parameters.max_epochs),
+            desc="Training progress",
+            unit="epoch",
+            total=training_parameters.max_epochs,
+            initial=1,
+        ):
+            val_epoch_metrics = None
+
+            val_epoch_metrics, _ = validate(
+                training_objects,
+                training_parameters,
+                epoch=epoch,
+                model_path=model_path.with_stem(
+                    f"{model_path.stem}_epoch{epoch + 1:03}"
+                ),
+                model_signature=model_signature,
+                log_mlflow=log_mlflow,
+            )
+
+            val_epoch_metrics_dict[epoch] = val_epoch_metrics
+
+            epoch_info_str = f"Epoch {epoch + 1}/{training_parameters.max_epochs}, Val Loss: {val_epoch_metrics.loss:.4f}"
+
+            tqdm.write(epoch_info_str)
+
+            pd.DataFrame(
+                [
+                    {
+                        "epoch": epoch,
+                        **val_epoch_metrics_dict[epoch].to_dict(
+                            split_labels=True,
+                            labels=training_parameters.label_names,
+                        ),
+                    }
+                    for epoch in sorted(val_epoch_metrics_dict)
+                ]
+            ).to_csv(model_path.with_name(f"{model_path.stem}_val_metrics.csv"))
+
+        pd.DataFrame(
+            [
+                {
+                    "epoch": epoch,
+                    **val_epoch_metrics_dict[epoch].to_dict(
+                        split_labels=True,
+                        labels=training_parameters.label_names,
+                    ),
+                }
+                for epoch in sorted(val_epoch_metrics_dict)
+            ]
+        ).to_csv(model_path.with_name(f"{model_path.stem}_val_metrics.csv"))
+
+        clear_memory()
+
+        if log_mlflow:
+            mlflow.log_param(
+                "best_train_metrics",
+                training_parameters.best_metrics["train"],
+            )
+            mlflow.log_param(
+                "best_val_metrics",
+                training_parameters.best_metrics["val"],
+            )
+            best_val_epoch = int(training_parameters.best_metrics["val"]["epoch"])
+            submit_validation_images_to_mflow(
+                training_objects=training_objects,
+                training_parameters=training_parameters,
+                epoch=best_val_epoch,
+            )
+
 
 def _validate_step(
     step: int,
